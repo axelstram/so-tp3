6563a16bb20240601c2f333ef3e13c214bbe3d8c
Sebastian Torres <softmindfreak@gmail.com>
2014-11-22 17:33:10 -0300

    Algunos cambios a los script provistos por la catedra para que funcionen en fedora y no confundan al usuario

--- code/code/csv_loader.py
+++ code/code/csv_loader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 from pymongo.connection import MongoClient
 import csv
 import json
@@ -10,23 +10,23 @@ reader = csv.DictReader(open('../data/redit.csv'),
             'reddit_id','number_of_upvotes','subreddit','number_of_downvotes',
             'localtime','score','number_of_comments','username'))
 
-conn = MongoClient() 
+conn = MongoClient()
 db = conn.reddit
 print "Cleaning DB collections %s.%s" % ("reddit", "posts")
 db.posts.remove()
 
 for it in reader:
     try:
-        it['total_votes'] = int(it['total_votes'] )
-        it['number_of_upvotes'] = int(it['number_of_upvotes'] )
-        it['number_of_downvotes'] = int(it['number_of_downvotes'] )
-        it['score'] = int(it['score'] )
-        it['number_of_comments'] = int(it['number_of_comments'] )
+        it['total_votes'] = int(it['total_votes'])
+        it['number_of_upvotes'] = int(it['number_of_upvotes'])
+        it['number_of_downvotes'] = int(it['number_of_downvotes'])
+        it['score'] = int(it['score'])
+        it['number_of_comments'] = int(it['number_of_comments'])
         db.posts.insert(it)
     except Exception as e:
         print e, "while inserting", it
 
 print "Inserted %d records" % db.posts.count()
-assert db.posts.count() == 269689, "Missing records on DB, inserted: %d" % db.posts.count()
+assert db.posts.count() == 132302, "Missing records on DB, inserted: %d" % db.posts.count()
 
 
--- code/code/restore.py
+++ code/code/restore.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
+"""Script para reinicializar la base de mongo, en caso de que se corrompiera"""
+
 import os
-""" Script para reinicializar la base de mongo, en caso de que se corrompiera"""
-os.remove('/var/lib/mongodb/mongod.lock')
-os.system('service mongodb restart')
+import platform
+
+if 'fedora' in platform.platform():
+    os.remove('/var/lib/mongo/mongod.lock')
+    os.system('service mongod restart')
+else:
+    os.remove('/var/lib/mongodb/mongod.lock')
+    os.system('service mongodb restart')
 
